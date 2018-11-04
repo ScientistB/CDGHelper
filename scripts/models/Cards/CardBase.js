15f059ec6c886259ed4a42ad3bb78e063845cbf2
ScientistB <scientist_b@live.jp>
2018-11-04 16:30:40 +0900

    refactor: CardBaseのクラス化
     #1

--- scripts/models/Cards/CardBase.js
+++ scripts/models/Cards/CardBase.js
@@ -1,73 +1,35 @@
 'use strict';
-
 /**
  * 各Cardクラスの基底クラス
  */
-var CardBase = (function () {
+var CardBase = /** @class */ (function () {
     /**
      * @constructor
      */
     function CardBase() {
         this.used = false;
     }
-
-    // public methods
-    CardBase.prototype = {
-        use_card: use_card,
-        get_name: get_name,
-        is_used: is_used,
-        is_match: is_match,
-        get_condition_str: get_condition_str,
-        _make_name: _make_name
-    };
-
-    return CardBase;
-
-    /**
-     * カードを使用済み状態にする
-     */
-    function use_card() {
-        this.used = true;
-    }
-
-    /**
-     * カードが使用済みであればtrueを返す
-     * @return {boolean}
-     */
-    function is_used() {
-        return this.used;
-    }
-
     /**
      * カードの名前の取得
      */
-    function get_name() {
+    CardBase.prototype.get_name = function () {
         if (angular.isUndefined(this.name)) {
             this.name = this._make_name();
         }
         return this.name;
-    }
-
+    };
     /**
-     * 与えられた数列の候補が、カードの条件と一致しているかどうかを返す抽象メソッド
-     * @param cand_nums: 候補の数字の配列
+     * カードが使用済みであればtrueを返す
      */
-    function is_match(cand_nums) {
-        throw new Error('Not Implemented');
-    }
-
+    CardBase.prototype.is_used = function () {
+        return this.used;
+    };
     /**
-     * カード共通の条件を表す文字列を返す抽象メソッド
+     * カードを使用済み状態にする
      */
-    function get_condition_str() {
-        throw new Error('Not Implemented');
-    }
-
-    /**
-     * カードの画面表示用の文字列を生成する抽象メソッド
-     * @protected
-     */
-    function _make_name() {
-        throw new Error('Not Implemented');
-    }
-})();
+    CardBase.prototype.use_card = function () {
+        this.used = true;
+    };
+    return CardBase;
+}());
+//# sourceMappingURL=CardBase.js.map
---
+++ scripts/models/Cards/CardBase.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * 各Cardクラスの基底クラス
+ */
+abstract class CardBase {
+    public used: boolean;
+    public name: string;
+
+    /**
+     * @constructor
+     */
+    protected constructor() {
+        this.used = false;
+    }
+
+    /**
+     * カードの名前の取得
+     */
+    public get_name() {
+        if (angular.isUndefined(this.name)) {
+            this.name = this._make_name();
+        }
+        return this.name;
+    }
+
+    /**
+     * カードが使用済みであればtrueを返す
+     */
+    public is_used(): boolean {
+        return this.used;
+    }
+
+    /**
+     * 与えられた数列の候補が、カードの条件と一致しているかどうかを返す抽象メソッド
+     */
+    public abstract is_match(cand_nums: number[]): boolean;
+
+    /**
+     * カードを使用済み状態にする
+     */
+    public use_card() {
+        this.used = true;
+    }
+
+    /**
+     * カード共通の条件を表す文字列を返す抽象メソッド
+     */
+    public abstract get_condition_str(): string;
+
+    /**
+     * カードの画面表示用の文字列を生成する抽象メソッド
+     * @protected
+     */
+    protected abstract _make_name(): string;
+}
