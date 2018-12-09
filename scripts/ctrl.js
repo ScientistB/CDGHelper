6a1eb86c6baf2662e9eda87be2ebda0c409b99fe
ScientistB <scientist_b@live.jp>
2018-12-09 17:15:49 +0900

    refactor: CDGServiceのメソッドのstatic化に対応
    #1

--- scripts/ctrl.js
+++ scripts/ctrl.js
@@ -3,8 +3,7 @@
  * コントローラクラス
  */
 var CDGController = /** @class */ (function () {
-    function CDGController(CDGService) {
-        this.CDGService = CDGService;
+    function CDGController() {
         this.show_candidates_status = false;
         this.card_groups = CDGService.init_cards();
         this.candidates = [];
@@ -13,8 +12,8 @@ var CDGController = /** @class */ (function () {
     }
     // カードと一致候補数の数を更新
     CDGController.prototype.use_card = function (card) {
-        this.CDGService.use_card(card, this.candidates);
-        this.match_candidates_num = this.CDGService.calc_match_candidates_num(this.candidates);
+        CDGService.use_card(card, this.candidates);
+        this.match_candidates_num = CDGService.calc_match_candidates_num(this.candidates);
     };
     // 候補の表示切り替え
     CDGController.prototype.show_candidates = function () {
@@ -22,7 +21,7 @@ var CDGController = /** @class */ (function () {
     };
     // カード名の取得
     CDGController.prototype.get_card_name = function (card) {
-        return this.CDGService.get_card_name(card);
+        return CDGService.get_card_name(card);
     };
     // カードが使用済みか
     CDGController.prototype.is_used = function (card) {
@@ -32,7 +31,6 @@ var CDGController = /** @class */ (function () {
     CDGController.prototype.get_candidate_nums = function (candidate) {
         return candidate.get_nums();
     };
-    CDGController.$inject = ['CDGService'];
     return CDGController;
 }());
 angular
--- scripts/ctrl.ts
+++ scripts/ctrl.ts
@@ -4,14 +4,12 @@
  * コントローラクラス
  */
 class CDGController {
-    public static $inject = ['CDGService'];
-
     public show_candidates_status: boolean; // 候補の表示状態
-    public card_groups: object[]; // カード
-    public candidates: object[]; // 候補
+    public card_groups: CardBase[][]; // カード
+    public candidates: Candidate[]; // 候補
     public match_candidates_num: number; // 生き残っている候補数
 
-    constructor(private CDGService) {
+    constructor() {
         this.show_candidates_status = false;
         this.card_groups = CDGService.init_cards();
         this.candidates = [];
@@ -20,23 +18,23 @@ class CDGController {
     }
 
     // カードと一致候補数の数を更新
-    public use_card(card: object) {
-        this.CDGService.use_card(card, this.candidates);
-        this.match_candidates_num = this.CDGService.calc_match_candidates_num(this.candidates);
+    public use_card(card: CardBase): void {
+        CDGService.use_card(card, this.candidates);
+        this.match_candidates_num = CDGService.calc_match_candidates_num(this.candidates);
     }
 
     // 候補の表示切り替え
-    public show_candidates() {
+    public show_candidates(): void {
         this.show_candidates_status = !this.show_candidates_status;
     }
 
     // カード名の取得
-    public get_card_name(card: object): string {
-        return this.CDGService.get_card_name(card);
+    public get_card_name(card: CardBase): string {
+        return CDGService.get_card_name(card);
     }
 
     // カードが使用済みか
-    public is_used(card): boolean {
+    public is_used(card: CardBase): boolean {
         return card.is_used();
     }
 
