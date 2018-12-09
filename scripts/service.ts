ec6a5d0cfb3130700ac13f9dd1b62031fe1f7a24
ScientistB <scientist_b@live.jp>
2018-12-09 17:11:06 +0900

    refactor: CDGServiceのクラス化
    #1

--- scripts/service.ts
+++ scripts/service.ts
@@ -1,11 +1,11 @@
 'use strict';
-var CDGService = /** @class */ (function () {
-    function CDGService() {
-    }
-    CDGService.get_card_name = function (card) {
+
+class CDGService {
+    public static get_card_name(card: CardBase): string {
         return card.get_name();
-    };
-    CDGService.init_cards = function () {
+    }
+
+    public static init_cards(): CardBase[][] {
         /**
          * 変数が1つのカードの初期化
          * @param constructor カードのコンストラクタ
@@ -13,13 +13,14 @@ var CDGService = /** @class */ (function () {
          * @param  max 最大値
          * @return カードオブジェクトの配列
          */
-        function single(constructor, min, max) {
-            var cards = [];
-            for (var i = min; i <= max; ++i) {
+        function single(constructor: any, min: number, max: number): CardBase[] {
+            const cards: CardBase[] = [];
+            for (let i = min; i <= max; ++i) {
                 cards.push(new constructor(i));
             }
             return cards;
         }
+
         /**
          * 変数が2つあるカードの初期化
          * @param constructor カードのコンストラクタ
@@ -27,10 +28,10 @@ var CDGService = /** @class */ (function () {
          * @param max 最大値
          * @return カードオブジェクトの配列
          */
-        function double(constructor, min, max) {
-            var cards = [];
-            for (var i = min; i <= max; ++i) {
-                for (var j = min; j <= max; ++j) {
+        function double(constructor: any, min: number, max: number): CardBase[] {
+            const cards: CardBase[] = [];
+            for (let i = min; i <= max; ++i) {
+                for (let j = min; j <= max; ++j) {
                     if (i !== j) {
                         cards.push(new constructor(i, j));
                     }
@@ -38,6 +39,7 @@ var CDGService = /** @class */ (function () {
             }
             return cards;
         }
+
         /**
          * 変数が２つあり、右辺と左辺を交換しても良いカードの初期化
          * @param constructor カードのコンストラクタ
@@ -45,17 +47,18 @@ var CDGService = /** @class */ (function () {
          * @param max 最大値
          * @return カードオブジェクトの配列
          */
-        function double_no_order(constructor, min, max) {
-            var cards = [];
-            for (var i = min; i <= max; ++i) {
-                for (var j = i + 1; j <= max; j++) {
+        function double_no_order(constructor: any, min: number, max: number): CardBase[] {
+            const cards: CardBase[] = [];
+            for (let i = min; i <= max; ++i) {
+                for (let j = i + 1; j <= max; j++) {
                     cards.push(new constructor(i, j));
                 }
             }
             return cards;
         }
+
         // 基本的なカードの生成
-        var card_groups = [
+        const card_groups = [
             single(CardExist, 1, 5),
             single(CardAll, 1, 5),
             double(CardLessThan, 1, 3),
@@ -63,38 +66,41 @@ var CDGService = /** @class */ (function () {
             double_no_order(CardNotEqual, 1, 3),
             single(CardDiff, 0, 3),
         ];
+
         // 単体カードの作成
-        var options = [];
+        const options = [];
         options.push(new CardDiffEqualPair());
         card_groups.push(options);
+
         return card_groups;
-    };
-    CDGService.init_candidates = function (candidates) {
-        for (var i = 1; i <= 5; i++) {
-            for (var j = 1; j <= 5; j++) {
-                for (var k = 1; k <= 5; k++) {
+    }
+
+    public static init_candidates(candidates: Candidate[]): void {
+        for (let i = 1; i <= 5; i++) {
+            for (let j = 1; j <= 5; j++) {
+                for (let k = 1; k <= 5; k++) {
                     candidates.push(new Candidate(i, j, k));
                 }
             }
         }
-    };
-    CDGService.use_card = function (card, candidates) {
+    }
+
+    public static use_card(card: CardBase, candidates: Candidate[]): void {
         card.use_card();
-        angular.forEach(candidates, function (cand) {
+        angular.forEach(candidates, (cand) => {
             cand.is_match(card);
         });
-    };
-    CDGService.calc_match_candidates_num = function (candidates) {
-        var count = 0;
-        angular.forEach(candidates, function (cand) {
-            if (cand.alive())
-                count++;
+    }
+
+    public static calc_match_candidates_num(candidates: Candidate[]): number {
+        let count = 0;
+        angular.forEach(candidates, (cand) => {
+            if (cand.alive()) count++;
         });
         return count;
-    };
-    return CDGService;
-}());
+    }
+}
+
 angular
     .module('CDGHelper')
     .service('CDGService', CDGService);
-//# sourceMappingURL=service.js.map
